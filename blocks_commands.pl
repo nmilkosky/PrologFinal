0617ab7f71e37fb5d7f7467cd6a5b098ecaec474
Angela Huang <huanga9@tcnj.edu>
2015-11-25 22:28:54 -0500

    Working version of blocks and commands

--- blocks_commands.pl
+++ blocks_commands.pl
@@ -4,7 +4,7 @@ block_statements --> block_statement.
 block_statements --> block_statements, block_statement.
 
 block_statement --> local_var_dec_statement.
-block_statement --> statement. 
+block_statement --> statement.
 
 local_var_dec_statement --> local_var_dec, [';'].
 local_var_dec --> type, var_declarators.
@@ -17,15 +17,15 @@ statement --> while_statement.
 statement --> for_statement.
 
 %left out synchronized statement
-simple_statement --> block. 
+simple_statement --> block.
 simple_statement --> empty_statement.
 simple_statement --> expression_statement. %dependent on expressions.pl
-simple_statement --> switch_statement. 
-simple_statement --> do_statement. 
-simple_statement --> break_statement. 
-simple_statement --> continue_statement. 
-simple_statement --> return_statement. 
-simple_statement --> continue_statement. 
+simple_statement --> switch_statement.
+simple_statement --> do_statement.
+simple_statement --> break_statement.
+simple_statement --> continue_statement.
+simple_statement --> return_statement.
+simple_statement --> continue_statement.
 simple_statement --> throws_statement.
 simple_statement --> try_statement.
 
@@ -43,7 +43,7 @@ expr_statement --> postDecrement_expr.
 expr_statement --> method_invocation.
 expr_statement --> create_class_instance_expr.
 
-%Allow optional brackets (add new rules)
+%Allow optional brackets (add new rules?)
 if_statement --> [if], ['('], expression, [')'], statement.
 if_else_statement --> [if], ['('], expression, [')'], statement, [else], statement.
 
@@ -63,23 +63,23 @@ switch_label --> [default], [':'].
 
 while_statement --> [while], ['('], expression, [')'], statement.
 
-do_statement --> [do], statement, [while] expression, [';'].
+do_statement --> [do], statement, [while], expression, [';'].
 
 %Are expression and for_counter optional?
-for_statement --> [for], ['('], for_init, [';'], expression, [';'], for_counter, [')'], statement. 
-for_statement --> [for], ['('], for_init, [';'], expression, [')'], statement. 
-for_statement --> [for], ['('], for_init, [';'], for_counter, [')'], statement. 
-for_statement --> [for], ['('], expression, [';'], for_counter, [')'], statement. 
-for_statement --> [for], ['('], for_init, [')'], statement. 
-for_statement --> [for], ['('], expression, [')'], statement. 
-for_statement --> [for], ['('], for_counter, [')'], statement. 
-for_statement --> [for], ['('], [')'], statement. 
+for_statement --> [for], ['('], for_init, [';'], expression, [';'], for_counter, [')'], statement.
+for_statement --> [for], ['('], for_init, [';'], expression, [')'], statement.
+for_statement --> [for], ['('], for_init, [';'], for_counter, [')'], statement.
+for_statement --> [for], ['('], expression, [';'], for_counter, [')'], statement.
+for_statement --> [for], ['('], for_init, [')'], statement.
+for_statement --> [for], ['('], expression, [')'], statement.
+for_statement --> [for], ['('], for_counter, [')'], statement.
+for_statement --> [for], ['('], [')'], statement.
 
 for_init --> expr_statements.
 for_init --> local_var_dec.
 
-expr_statements --> expr_statement. 
-expr_statements --> expr_statements, expr_statement. 
+expr_statements --> expr_statement.
+expr_statements --> expr_statements, expr_statement.
 
 break_statement --> [break], [';'].
 break_statement --> [break], identifier, [';'].
@@ -97,4 +97,4 @@ try_statement --> [try], block, finally.
 catches --> catch_clause.
 catches --> catches, catch_clause.
 catch_clause --> [catch], ['('], formal_parameter, [')'], block.
-finally --> [finally], block. 
+finally --> [finally], block.
