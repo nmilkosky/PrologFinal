65e3a79909d9a474a17f883b9750ef4fcb9cbc44
Nate Milkosky <nmilkosky@gmail.com>
2015-12-08 23:49:01 -0500

    Added new test file

---
+++ test3.java
@@ -0,0 +1,14 @@
+abstract class test3 {
+
+	public int x ;
+	public int y ;
+
+	public void fun ( int var ) {
+		if ( var > x )
+			var = var + 1 ;
+	}
+
+	abstract void fun2 ( ) ;
+	
+	abstract int fun3 ( ) ;
+}
